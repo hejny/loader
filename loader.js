7b40dc02275e0dd2dcc0b447def2875296eb41ff
Pavol Hejny <me@pavolhejny.com>
2018-05-26 12:45:35 +0200

    loader from ftp

---
+++ loader.js
@@ -0,0 +1,47 @@
+function loadBookViewerApp(version, callback) {
+
+
+
+
+
+//todo relative url
+    var jqxhr = $.getJSON("https://cdn.h-edu.cz/book-viewer-embeded/loader.php?version=" + version, function () {
+        //console.log( "success" );
+    })
+        .done(function (response) {
+
+            console.log( "Loading BookViewerApp version "+response.data.version );
+
+	    response.data.assets.js.forEach(function(url){
+		     //todo without jQuery
+		    jQuery.ajax({
+		        url: url,
+		        dataType: 'script',
+		        success: function () {
+		            callback(window.BookViewerApp);
+		        },
+		        async: true
+		    });
+	    });
+
+	    response.data.assets.css.forEach(function(url){
+		    var head = document.getElementsByTagName('head')[0];
+		    var link = document.createElement('link');
+		    link.rel = 'stylesheet';
+		    link.type = 'text/css';
+		    link.href = url;
+		    link.media = 'all';
+		    head.appendChild(link);
+	    });
+
+
+        })
+        .fail(function () {
+            throw new Error( "Error while loading BookViewerApp version "+version );
+        })
+        .always(function () {
+            //console.log( "complete" );
+        });
+
+
+}
---
+++ loader.php
@@ -0,0 +1,65 @@
+<?php
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
+define('BASE_URL','https://cdn.h-edu.cz/book-viewer-embeded/');
+
+if (preg_match('/^(?:(\d+)\.)?(?:(\d+)\.)?(\*|\d+)$/', $_GET['version'])) {
+	$version = $_GET['version'];
+	$versions = array_reverse(glob("versions/$version"));//todo secure?
+
+	if(count($versions)!==0){
+		$version = basename($versions[0]);
+	}else{
+	    //todo REST JSON + 404
+	    die('version not exists');
+	}
+    
+} else {
+    //todo REST JSON + 400
+    die('?version=[semantic version]');
+}
+
+
+
+if (file_exists("versions/$version")) {
+//todo not found
+
+ 
+    $js = glob("versions/$version/**/*.js");
+    $css = glob("versions/$version/**/*.css");
+
+    foreach($js as &$url){
+	$url=BASE_URL.$url;
+    }
+    foreach($css as &$url){
+	$url=BASE_URL.$url;
+    }
+
+
+    $response = array(
+        'status' => 'ok',
+        'data' =>
+            array(
+                'version' => $version,
+                'assets' => array(
+                    //todo maybe relative url
+                    'js' => $js,
+		    'css' => $css
+                )
+            )
+    );
+
+    header("Content-type: application/json");
+    echo json_encode($response, JSON_PRETTY_PRINT);
+
+} else {
+    //todo REST JSON + 404
+    die('unexpected error');
+
+}
+
+
+?>
