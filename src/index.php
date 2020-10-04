cce5af78a4b3e5b6deeab839aa7f4abc0b032cc2
Jonáš Rosecký <j.o.n.d.a@centrum.cz>
2020-10-04 20:37:04 +0300

    Fixed path to versions

--- src/index.php
+++ src/index.php
@@ -46,7 +46,7 @@ define('BASE_URL', explode('loader.php', $selfUrl)[0]);
 
 if (preg_match('/^(?:(\d+)\.)?(?:(\d+)\.)?(\*|\d+)$/', $_GET['version'])) {
     $version = $_GET['version'];
-    $versions = glob("versions/$version"); //todo secure?
+    $versions = glob("../versions/$version"); //todo secure?
     usort($versions, function ($version1, $version2) {
         return -version_compare(basename($version1), basename($version2));
     });
